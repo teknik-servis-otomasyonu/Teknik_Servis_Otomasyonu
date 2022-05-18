8d415d14907e37664f5ad7a85ddf6d01a180d151
Emre <104350586+66Emre@users.noreply.github.com>
2022-05-18 18:42:27 +0300

    Update otomasyon giriş.py

--- otomasyon giriş.py
+++ otomasyon giriş.py
@@ -1,5 +1,21 @@
 #projenın genel çaışma durumu ve kurguya giriş
 #(personel bilgileri talep bilgileri vb.)
 
-gençler bugün haberleşelim projeye biraz hız kazandıralım artık vakit daralıyor.
+#ana ekran sınıfı
+class Anaekran:
+        def __init__(self,personel_id):
+        self.personel_id = personel_id
+
+        self.karsilama()
+
+        def karsilama(self):  
+        # self.setupUi(self)
+        #menülerin yönlendirilmesi
+        self.actionTalep_A.triggered.connect(self.talep_ekle)
+        self.actionPersonel_Ekle.triggered.connect(self.personel_ekle)
+        self.actionDevam_Eden_Talepler.triggered.connect(self.karsilama)
+        self.actionKapanan_Talepler.triggered.connect(self.fkapanan)
+        self.actionPersnel_G_ncelle_Sil.triggered.connect(self.fpersonelgs)
+        self.tableWidget.setRowCount(0)
+        self.facik_isler()
 

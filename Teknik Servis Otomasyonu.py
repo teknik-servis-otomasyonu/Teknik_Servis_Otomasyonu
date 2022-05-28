ea6ad356ede3b2f09624c3a04fbb317eb14d90de
Emre <104350586+66Emre@users.noreply.github.com>
2022-05-28 15:59:10 +0300

    Add files via upload

--- Teknik Servis Otomasyonu.py
+++ Teknik Servis Otomasyonu.py
@@ -1,3 +1,6 @@
+from pydoc import TextRepr
+
+
 class Otomasyon():
     def __init__(self,ad):
         self.ad = ad
@@ -21,10 +24,12 @@ class Otomasyon():
             self.arızaAdres()
         if secim == 7:
             self.arızaTarih()
+        if secim == 8:
+            self.yetkilipersonel()
     def menuSecim(self):
-        secim =int( input("**** {} hoş geldiniz ****\n\n 1-İsim Giriniz: \n 2-Soyisim Giriniz:\n 3-Arıza Ekle:\n 4-Arıza Çıkar:\n 5-Arıza Cinsi:\n 6-Arıza Tarih:".format(self.ad)))
-        while secim < 1 or secim > 6:
-            secim = int(input("Lütfen 1 - 6 arasında belirtilen seçeneklerden birini giriniz!"))
+        secim =int( input("**** {} hoş geldiniz ****\n\n 1-İsim Giriniz: \n 2-Soyisim Giriniz:\n 3-Arıza Ekle:\n 4-Arıza Çıkar:\n 5-Arıza Cinsi:\n 6-Arıza Tarih:\n 7-yetkilipersonel:\n\nseçiminizi giriniz:".format(self.ad)))
+        while secim < 1 or secim > 7:
+            secim = int(input("Lütfen 1 - 7 arasında belirtilen seçeneklerden birini giriniz!"))
 
      
         return secim
@@ -33,6 +38,7 @@ class Otomasyon():
     def soyisimGir(self):
         pass
     def arızaEkle(self):
+        id = 1
         isim = input("Adınızı Giriniz: ") 
         soyisim = input("Soyadınızı Giriniz: ") 
         ürünadı = input("Arıza kaydı oluşturacağınız ürün adı: ")
@@ -40,10 +46,13 @@ class Otomasyon():
         açıkadres = input("Adresinizi  Giriniz: ") 
         garantisüresi = input("garanti süresini giriniz")
         tarih = input("talepte bulunduğunuz tarihi giriniz")
-        with open("Arızatalebi.txt","r") as dosya:
+        with open("Arızatalebi.txt","a+") as dosya:
             arızatalebiListesi = dosya.readlines()   
+        
         if len(arızatalebiListesi) == 0:
             id = 1
+            with open("Arızatalebi.txt","a+") as dosya:
+                dosya.write("{})-{}-{}-{}-{}-{}-{}\n".format(id,isim,soyisim,ürünadı,ürünarıza,açıkadres,garantisüresi,tarih))
         else:
             with open("Arızatalebi.txt","r") as dosya:
                 id = int(dosya.readlines()[-1].split(")")[0]) + 1
@@ -52,7 +61,11 @@ class Otomasyon():
             dosya.write("{}){}-{}-{}-{}-{}-{}-{}\n".format(id,isim,soyisim,ürünadı,ürünarıza,açıkadres,garantisüresi,tarih))
     
     def arızaCikar(self):
-        pass
+        with open ("Arızatalebi.txt","r") as dosya:
+            arızatalebi = dosya.readlines()
+            arızatalebiListesi = dosya.readlines()
+        for arızatalebi in arızatalebiListesi:
+                print(arızatalebi)
     def arızaCinsi(self):
         pass 
     def arızaAdres(self):

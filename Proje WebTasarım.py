a391b4a4e472b30f4e2eb8ddd4827e2fc7c88c95
alpdogan04 <104662286+alpdogan04@users.noreply.github.com>
2022-05-29 21:19:31 +0300

    Yorumlar kısmı deneme

---
+++ Proje WebTasarım.py
@@ -0,0 +1,9 @@
+#print("Hello World!")
+
+def Teknik_Servis_Otomasyonu(self):
+    Teknik_Servis_Otomasyonu = 'Yorumlar'
+    Teknik_Servis_Otomasyonu(input('Ürün hakkında düşündüğünüzü buraya giriniz: '))
+    
+    print(Teknik_Servis_Otomasyonu)
+    return
+

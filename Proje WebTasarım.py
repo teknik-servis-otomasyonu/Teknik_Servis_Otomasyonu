74cfa1cdb5f0bf3558172f7d319d04934b5b7c0b
Ercan Kara <100122465+ErcanKara18@users.noreply.github.com>
2022-06-17 01:36:20 +0300

    Delete Proje WebTasarım.py

--- Proje WebTasarım.py
+++
@@ -1,9 +0,0 @@
-#print("Hello World!")
-
-def Teknik_Servis_Otomasyonu(self):
-    Teknik_Servis_Otomasyonu = 'Yorumlar'
-    Teknik_Servis_Otomasyonu(input('Ürün hakkında düşündüğünüzü buraya giriniz: '))
-    
-    print(Teknik_Servis_Otomasyonu)
-    return
-
